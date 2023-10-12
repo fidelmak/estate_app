58d501b477a5278a16ca2bd903f300f25acb8624
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-10-12 15:45:22 +0100

    Changes to be committed:

--- lib/practise/data/dummy.dart
+++ lib/practise/data/dummy.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 import '../myCategory.dart';
 
 List<Category> availableCat = [
-  Category(id: '1', title: "my fist item"),
-  Category(id: '1', title: "my fist item"),
-  Category(id: '1', title: "my fist item"),
-  Category(id: '1', title: "my fist item"),
-  Category(id: '1', title: "my fist item"),
+  Category(id: '1', title: "Italian", color: Colors.red),
+  Category(id: '1', title: "French", color: Colors.yellow),
+  Category(id: '1', title: "Brazil", color: Colors.pink),
+  Category(id: '1', title: "African", color: Colors.blue),
+  Category(id: '1', title: "Chinese", color: Colors.lime),
 ];
