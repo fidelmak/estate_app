c20eebd5d566e5af39a68ef8213b4fe78171705b
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-10-14 17:57:56 +0100

    On branch main

---
+++ lib/practise/widget/category_grid.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../myCategory.dart';
+
+class CategoryWidget extends StatelessWidget {
+  const CategoryWidget({super.key, required this.category});
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          category.color.withOpacity(0.55),
+          category.color.withOpacity(0.9)
+        ], begin: Alignment.topLeft, end: Alignment.topRight),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context)
+            .textTheme
+            .titleLarge
+            .copyWith(color: Theme.of(context).colorScheme.onBackground),
+      ),
+    );
+  }
+}
